afdaabd66590f189ab55f4803dfafd82c382a220
Nicolas Martinez V <nmartinezv@icloud.com>
2026-05-11 20:00:58 -0400

    feat: add build platform

--- modules/modules/linux/bootstrap.nix
+++ modules/modules/linux/bootstrap.nix
@@ -27,6 +27,11 @@ in {
           self.commonModules.state-version
 
           {
+            nixpkgs = {
+              buildPlatform = "x86_64-linux";
+              hostPlatform = "aarch64-linux";
+            };
+
             services.openssh = {
               enable = true;
               settings.PermitRootLogin = "yes";
