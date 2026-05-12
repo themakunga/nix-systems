6f7037009e85c66ebc4fa21f49273027b176962a
Nicolas Martinez V <nmartinezv@icloud.com>
2026-05-12 16:01:30 -0400

    fix: single system builder

--- modules/hosts/linux/sd-image/default.nix
+++ modules/hosts/linux/sd-image/default.nix
@@ -4,16 +4,17 @@
   ...
 }: let
   inherit (inputs) nixpkgs sops-nix;
+  inherit (self) commonModules;
 in {
   flake.nixosConfigurations.sd-image = {system}:
     nixpkgs.lib.nixosSystem {
-      inherit system;
+      system = "aarch64-linux";
       specialArgs = {inherit inputs;};
       modules = [
         sops-nix.nixosMopdules.sops
         "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
 
-        self.commonConfigs.state-version
+        commonModules.state-version
         {
           network.hostName = "sdimage-install";
           nixpkgs = {
