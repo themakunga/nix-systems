01846357ffe3681c0a2f9bf14b1beffbde97a5f1
Nicolas Martinez V <nmartinezv@icloud.com>
2026-05-13 00:12:35 -0400

    fix: create single image builder

--- modules/hosts/linux/sd-image/default.nix
+++ modules/hosts/linux/sd-image/default.nix
@@ -3,52 +3,50 @@
   inputs,
   ...
 }: let
-  inherit (inputs) nixpkgs sops-nix;
+  inherit (inputs) nixpkgs sops-nix secrets;
   inherit (self) commonModules;
 in {
-  flake.nixosConfigurations.sd-image = {system}:
-    nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      specialArgs = {inherit inputs;};
-      modules = [
-        sops-nix.nixosMopdules.sops
-        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+  flake.nixosConfigurations.sd-image = nixpkgs.lib.nixosSystem {
+    system = "aarch64-linux";
+    specialArgs = {inherit inputs;};
+    modules = [
+      sops-nix.nixosModules.sops
+      "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
 
-        commonModules.state-version
-        {
-          network.hostName = "sdimage-install";
-          nixpkgs = {
-            buildPlatform = "x86_64-linux";
-            hostPlatform = "aarch64-linux";
+      commonModules.state-version
+      {
+        networking.hostName = "sdimage-install";
+        nixpkgs = {
+          buildPlatform = "x86_64-linux";
+          hostPlatform = "aarch64-linux";
+        };
+
+        services.openssh = {
+          enable = true;
+          settings = {
+            PermitRootLogin = "yes";
           };
+        };
 
-          serives.openssh = {
-            enable = true;
-            settings = {
-              PermitRootLogin = "yes";
-            };
-          };
+        users.users.root.openssh.authorizedKeys.keys = [
+          (builtins.readFile "${secrets}/public-keys/outer-heaven.pub")
+        ];
 
-          users.users.root.openssh.authorizedKeys.keys =
-            if builtins.isList authoriedKeys
-            then authorizedKeys
-            else [authjorisedKeys];
+        nix.settings.experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
 
-          nix.settings.experimental-features = [
-            "nix-command"
-            "flakes"
-          ];
+        sdImage.compressImage = true;
 
-          sdImage.compressImage = true;
+        image.fileName = "sd-image-aarch64-generic.img";
 
-          image.fileName = "sd-image-aarch64-generic.img";
-
-          environment.systemPackages = with inputs.nixpkgs.legacyPackages.${system}; [
-            git
-            curl
-            disko
-          ];
-        }
-      ];
-    };
+        environment.systemPackages = with inputs.nixpkgs.legacyPackages.aarch64-linux; [
+          git
+          curl
+          disko
+        ];
+      }
+    ];
+  };
 }
