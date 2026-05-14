b1072ac50da909ad3ed208dcf1a192eec36850cc
Nicolas Martinez V <nmartinezv@icloud.com>
2026-05-14 12:28:54 -0400

    feat: disko images

--- modules/modules/linux/disko.nix
+++ modules/modules/linux/disko.nix
@@ -1,6 +1,35 @@
 {
   flake.diskModules = {
-    glaDOS = {
+    sd-image = {
+      disco.devices = {
+        disk = {
+          main = {
+            type = "disk";
+            device = "/dev/mmcblk0";
+            content = {
+              type = "gpt";
+              partitions = {
+                boot = {
+                  size = "512M";
+                  type = "EF00";
+                  content = {
+                    type = "filesystem";
+                    format = "vfat";
+                    mountpoint = "/boot";
+                  };
+                };
+                root = {
+                  size = "100%";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    wheatley = {
       disko.devices = {
         disk = {
           main = {
@@ -33,7 +62,7 @@
         };
       };
     };
-    cornholio = {
+    glaDOS = {
       disko.devices = {
         disk = {
           main = {
