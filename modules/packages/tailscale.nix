dbbfb0d359a08e265a9cade3e3784dc4612342cb
Nicolas Martinez V <nmartinezv@icloud.com>
2026-05-27 17:24:53 -0400

    fix: typo

--- modules/packages/tailscale.nix
+++ modules/packages/tailscale.nix
@@ -2,7 +2,7 @@
   flake.commonModules.tailscale = {config, ...}: {
     services.tailscale = {
       enable = true;
-      authKeyFile = config.sops.secrets."tailscale_auth_key".path;
+      authKeyFile = config.sops.secrets."tailscale/auth_key".path;
     };
 
     authKeyFile = config.sops.secrets."tailscale/auth_key".path;
