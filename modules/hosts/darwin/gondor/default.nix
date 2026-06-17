5d7e89d8dd89e943802ae372f911501a1f98668a
Nicolas Martinez V <nmartinezv@icloud.com>
2026-06-17 10:20:38 -0400

    feat: gondor host

--- modules/hosts/darwin/gondor/default.nix
+++ modules/hosts/darwin/gondor/default.nix
@@ -1,40 +1,53 @@
 {
-  inputs,
   self,
+  inputs,
   ...
-}:
-let
-  inherit (inputs)
+}: let
+  inherit
+    (inputs)
     nix-darwin
     nix-homebrew
     home-manager
-    sops-nix
     ;
-  inherit (self)
-    commonModules
+  inherit
+    (self)
     darwinModules
+    commonModules
+    profileModules
     ;
-in
-{
-  flake.darwinConfigurations.big-shell = nix-darwin.lib.darwinSystem {
-    specialArgs = { };
+in {
+  flake.darwinConfigurations.gondor = nix-darwin.lib.darwinSystem {
+    specialArgs = {inherit self inputs;};
     system = "aarch64-darwin";
+
     modules = [
       commonModules.settings
-
       darwinModules.common
-
-      sops-nix.darwinModules.sops
+      commonModules.home-manager-config
 
       nix-homebrew.darwinModules.nix-homebrew
       {
-        networking.hostName = "kanagawa";
-
-        services.openssh.enable = true;
-
+        nerworking.hpstName = "gondor";
+        services.opensh.enable = true;
         nix-homebrew.user = "nicolas";
       }
+
+      profileModules.aragon.system
+      profileModules.aragon.darwin
+
       home-manager.darwinModules.home-manager
+      {
+        home-manager = {
+          extraSpecialArgs = {inherit self inputs;};
+          uses.nicolas = {
+            imports = [
+              profileModules.aragon.user
+              profileModules.boromir.user
+              profileModules.faramir.user
+            ];
+          };
+        };
+      }
     ];
   };
 }
