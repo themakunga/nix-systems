75572525727828885340883e72d238b9ebc2ff9c
Nicolas Martinez V <nmartinezv@icloud.com>
2026-06-17 00:00:53 -0400

    feat: sops module ru

--- modules/modules/common/sops.nix
+++ modules/modules/common/sops.nix
@@ -1,9 +1,103 @@
 {inputs, ...}: let
   inherit (inputs) sops-nix secrets;
 in {
-  flake.commonModules.sops = {
-    gpg = {
-      pkgs,
+  flake.commonModules = {
+    sops = {
+      gpg = {
+        pkgs,
+        lib,
+        ...
+      }: let
+        inherit
+          (lib)
+          mkEnableOption
+          mkOption
+          types
+          mkIf
+          concatMapStringsSep
+          ;
+      in {
+        home-manager.sharedModules = [
+          (
+            {config, ...}: let
+              cfg = config.programs.sops.gpg;
+              inherit (config.lib.home-manager.dag) entryAfter;
+            in {
+              options.programs.sops.gpg = {
+                enable = mkEnableOption "Declarative GPG key importer";
+                keys = mkOption {
+                  description = "Private and Public GPG Keys";
+                  default = [];
+                  type = types.listOf (
+                    types.submodule {
+                      options = {
+                        name = mkOption {type = types.str;};
+                        publicKey = mkOption {type = types.str;};
+                        privateKey = mkOption {type = types.str;};
+                      };
+                    }
+                  );
+                };
+              };
+              config = mkIf cfg.enable {
+                home.activation.importSopsGpg = entryAfter ["writeBoundary"] (
+                  concatMapStringsSep "\n" (key: ''
+                    if [ -f "${key.publicKey}" ]; then
+                      echo "[GPG] import public key to host: ${key.name}..."
+                      ${pkgs.gnupg}/bin/gpg --import "${key.publicKey}"
+                    fi
+                    if [ -f "${key.privateKey}" ]; then
+                      echo "[GPG] import private key to host: ${key.name}..."
+                      ${pkgs.gnupg}/bin/gpg --import "${key.privateKey}"
+                    fi
+                  '')
+                  cfg.keys
+                );
+              };
+            }
+          )
+        ];
+      };
+
+      shared-secrets = {config, ...}: let
+        inherit (config.home) homeDirectory;
+        host = config.networking.hostName or "default";
+        commonSopsFile = "${secrets}/common.yaml";
+        hostSopsFile = "${secrets}/hosts/${host}.yaml";
+      in {
+        imports = [sops-nix.homeManagerModules.sops];
+
+        sops = {
+          age = {
+            sshKeyPath = ["${homeDirectory}/.ssh/id_ed25519"];
+            generateKey = false;
+          };
+
+          defaultSopsFile =
+            if builtins.pathExists hostSopsFile
+            then hostSopsFile
+            else commonSopsFile;
+
+          secrets = {
+            "wifi/AMANDA" = {
+              sopsFile = commonSopsFile;
+            };
+            "wifi/42Dev_5G" = {
+              sopsFile = commonSopsFile;
+            };
+            "wifi/42Devs" = {
+              sopsFile = commonSopsFile;
+            };
+            "tailscale/aut_token" = {
+              sopsFile = commonSopsFile;
+            };
+          };
+        };
+      };
+    };
+
+    git-identity = {
+      config,
       lib,
       ...
     }: let
@@ -13,91 +107,103 @@ in {
         mkOption
         types
         mkIf
-        concatMapStringSeq
+        mkMerge
+        optionalAttrs
+        mapAttrsToList
         ;
+      cfg = config.programs.git-identity;
     in {
-      home-manager.sharedModules = [
-        (
-          {config, ...}: let
-            cfg = config.programs.sops.gpg;
-            inherit (config.lib.home-manager.dag) entryAfter;
-          in {
-            options.programs.sops.gpg = {
-              enable = mkEnableOption "Declarative GPG key importer";
-              keys = mkOption {
-                description = "Private and Public GPG Keys";
-                default = [];
-                type = types.listOf (
-                  types.submodule {
-                    optons = {
-                      name = mkOption {
-                        type = types.str;
-                      };
-                      publicKey = {
-                        type = types.str;
-                      };
-                      privateKey = {
-                        type = types.str;
-                      };
-                    };
-                  }
-                );
-              };
-            };
-            config = mkIf cfg.enable {
-              home.activation.importSopsGpg = entryAfter ["writeBounder"] (
-                concatMapStringSeq "\n" (key: ''
-                  if [ -f "${key.publicKey}" ]; then
-                    echo "[GPG] import public key to host: ${key.name}..."
-                    ${pkgs.gnupg}/bin/gpg --import "${key.publicKey}"
-                  fi
-                  if [ -f "${key.privateKey}" ]; then
-                    echo "[GPG] import private key to host: ${key.name}..."
-                    ${pkgs.gnupg}/bin/gpg --import "${key.privateKey}"
-                  fi
+      options.programs.git-identity = {
+        enable = mkEnableOption "Gestor de identidad de Git parametrizado";
 
-                '')
-                cfg.keys
-              );
+        global = {
+          enable = mkEnableOption "Identidad global por defecto";
+          realName = mkOption {type = types.str;};
+          email = mkOption {type = types.str;};
+          gpg = {
+            enable = mkEnableOption "Firmado global con GPG";
+            keyId = mkOption {
+              type = types.nullOr types.str;
+              default = null;
             };
-          }
-        )
-      ];
-    };
-    shared-secrets = {config, ...}: let
-      inherit (config.home) homeDirectory;
-      host = config.networking.hostName or "default";
-      commonSopsFile = "${secrets}/common.yaml";
-      hostSopsFile = "${secrets}/hosts/${host}.yaml";
-    in {
-      imports = [
-        sops-nix.homeManagerModules.sops
-      ];
-
-      sops = {
-        age = {
-          sshKeyPath = ["${homeDirectory}/.ssh/id_ed25519"];
-          generateKet = false;
+          };
+          ssh = {
+            enableAuth = mkEnableOption "Auth SSH global";
+            privateKeyPath = mkOption {
+              type = types.nullOr types.str;
+              default = null;
+            };
+          };
         };
 
-        defaultSopsFile =
-          if builtins.pathExists hostSopsFile
-          then hostSopsFile
-          else commonSopsFile;
+        workspaces = mkOption {
+          default = {};
+          description = "Configuraciones de Git aplicadas solo en carpetas específicas";
+          type = types.attrsOf (
+            types.submodule {
+              options = {
+                directory = mkOption {type = types.str;};
+                realName = mkOption {type = types.str;};
+                email = mkOption {type = types.str;};
+                gpg = {
+                  enable = mkEnableOption "Firmado GPG para este workspace";
+                  keyId = mkOption {
+                    type = types.nullOr types.str;
+                    default = null;
+                  };
+                };
+                ssh = {
+                  enableAuth = mkEnableOption "Auth SSH para este workspace";
+                  privateKeyPath = mkOption {
+                    type = types.nullOr types.str;
+                    default = null;
+                  };
+                };
+              };
+            }
+          );
+        };
+      };
 
-        secrets = {
-          "wifi/AMANDA" = {
-            sopsFile = commonSopsFile;
+      config = mkIf cfg.enable {
+        programs.git = {
+          enable = true;
+
+          userName = mkIf cfg.global.enable cfg.global.realName;
+          userEmail = mkIf cfg.global.enable cfg.global.email;
+
+          signing = mkIf (cfg.global.enable && cfg.global.gpg.enable) {
+            key = cfg.global.gpg.keyId;
+            signByDefault = true;
           };
-          "wifi/42Dev_5G" = {
-            sopsFile = commonSopsFile;
-          };
-          "wifi/42Devs" = {
-            sipsFile = commonSopsFile;
-          };
-          "tailscale/aut_token" = {
-            sopsFile = commonSopsFile;
-          };
+
+          extraConfig = mkMerge [
+            (mkIf (cfg.global.enable && cfg.global.ssh.enableAuth) {
+              core.sshCommand = "ssh -i ${cfg.global.ssh.privateKeyPath} -o IdentitiesOnly=yes";
+            })
+          ];
+
+          includes =
+            mapAttrsToList (_name: ws: {
+              condition = "gitdir:${ws.directory}/";
+              contents = {
+                user =
+                  {
+                    name = ws.realName;
+                    inherit (ws) email;
+                  }
+                  // optionalAttrs ws.gpg.enable {
+                    signingkey = ws.gpg.keyId;
+                  };
+                commit = optionalAttrs ws.gpg.enable {
+                  gpgsign = true;
+                };
+                core = optionalAttrs ws.ssh.enableAuth {
+                  sshCommand = "ssh -i ${ws.ssh.privateKeyPath} -o IdentitiesOnly=yes";
+                };
+              };
+            })
+            cfg.workspaces;
         };
       };
     };
