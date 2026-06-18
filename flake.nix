f64277aea90e53cec5a3ab505373c856969bf1b2
Nicolas Martinez V <nmartinezv@icloud.com>
2026-06-18 10:15:35 -0400

    feat: update flakes

--- flake.nix
+++ flake.nix
@@ -10,11 +10,11 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     nix-darwin = {
-      url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
