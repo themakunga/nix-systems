9f460172aaea1e544626428b9f484e65193419e7
Nicolas Martinez V <nmartinezv@icloud.com>
2026-06-26 11:44:54 -0400

    feat: mediaserver msf

---
+++ modules/profiles/mediaserver.nix
@@ -0,0 +1,58 @@
+{self, ...}: let
+  inherit (self) commonModules;
+in {
+  flake.profileModules.mediaserver = {
+    sops.secrets = {
+      "profiles/mediaserver/ssh/private_key" = {};
+      "profiles/mediaserver/gpg/private_key" = {};
+      "profiles/mediaserver/gpg/public_key" = {};
+      "profiles/mediaserver/gpg/key_id" = {};
+    };
+
+    my.userProfiles.mediaserver.homeManager = {
+      # pkgs,
+      osConfig,
+      ...
+    }: {
+      imports = [
+        commonModules.git-identity
+      ];
+
+      programs = {
+        sops.gpg = {
+          enable = true;
+          keys = [
+            {
+              name = "mediaserver-key";
+              publicKey = osConfig.sops.secrets."profiles/mediaserver/gpg/public_key".path;
+              privateKey = osConfig.sops.secrets."profiles/mediaserver/gpg/private_key".path;
+            }
+          ];
+        };
+        git-identity = {
+          enable = true;
+          workspaces.mediaserver = {
+            directory = "~/Projects";
+            realName = "Nicolas Villarroel";
+            email = "nmartinezv@icloud.com";
+            gpg = {
+              enable = true;
+              keyId = osConfig.sops.secrets."profiles/mediaserver/gpg/key_id";
+            };
+            ssh = {
+              enableAuth = true;
+              privateKey = osConfig.sops.secrets."profiles/mediaserver/ssh/private_key".path;
+            };
+          };
+        };
+      };
+
+      # home.packages = with pkgs; [ ];
+
+      services.gpg-agent = {
+        enable = true;
+        enableSshSupport = true;
+      };
+    };
+  };
+}
