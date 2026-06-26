8d6c12112b440bee5ad40f329556092297a70aef
Nicolas Martinez V <nmartinezv@icloud.com>
2026-06-26 11:54:33 -0400

    feat: steam deck profile

---
+++ modules/profiles/steamdeck.nix
@@ -0,0 +1,58 @@
+{self, ...}: let
+  inherit (self) commonModules;
+in {
+  flake.profileModules.steamdeck = {
+    sops.secrets = {
+      "profiles/steamdeck/ssh/private_key" = {};
+      "profiles/steamdeck/gpg/private_key" = {};
+      "profiles/steamdeck/gpg/public_key" = {};
+      "profiles/steamdeck/gpg/key_id" = {};
+    };
+
+    my.userProfiles.deck.homeManager = {
+      # pkgs,
+      osConfig,
+      ...
+    }: {
+      imports = [
+        commonModules.git-identity
+      ];
+
+      programs = {
+        sops.gpg = {
+          enable = true;
+          keys = [
+            {
+              name = "steamdeck-key";
+              publicKey = osConfig.sops.secrets."profiles/steamdeck/gpg/public_key".path;
+              privateKey = osConfig.sops.secrets."profiles/steamdeck/gpg/private_key".path;
+            }
+          ];
+        };
+        git-identity = {
+          enable = true;
+          workspaces.steamdeck = {
+            directory = "~/Projects";
+            realName = "Nicolas Villarroel";
+            email = "nmartinezv@icloud.com";
+            gpg = {
+              enable = true;
+              keyId = osConfig.sops.secrets."profiles/steamdeck/gpg/key_id";
+            };
+            ssh = {
+              enableAuth = true;
+              privateKey = osConfig.sops.secrets."profiles/steamdeck/ssh/private_key".path;
+            };
+          };
+        };
+      };
+
+      # home.packages = with pkgs; [ ];
+
+      services.gpg-agent = {
+        enable = true;
+        enableSshSupport = true;
+      };
+    };
+  };
+}
