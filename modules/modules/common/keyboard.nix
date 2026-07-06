3e6b1df6dd469ec50a66878d4ae7a7ab975bd4fe
Nicolas Martinez V <nmartinezv@icloud.com>
2026-07-06 16:29:51 -0400

    feat: keyboard module

---
+++ modules/modules/common/keyboard.nix
@@ -0,0 +1,38 @@
+{lib, ...}: {
+  flake.commonModules.ketboard = {
+    config,
+    pkgs,
+    ...
+  }: let
+    inherit (lib) mkEnableOption mkIf mkMerge;
+    cfg = config.my.keyboard;
+    inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
+  in {
+    options.my.keyboard = {
+      enable = mkEnableOption "Common keyboard config";
+    };
+
+    config = mkIf cfg.enable (mkMerge [
+      (mkIf isDarwin {
+        system = {
+          keyboard = {
+            enableKeyMapping = true;
+            remapCapsLockToControl = true;
+          };
+          default.NSGlobalDomain = {
+            AppleLanguages = ["en-US" "es-CL"];
+            AppleLocale = "en_US";
+          };
+        };
+      })
+      (mkIf isLinux {
+        console.keyMap = "us";
+
+        services.xserver.xkb = {
+          layout = "us,latam";
+          options = "ctrl:nocaps,grp:win_spaccce_toggle";
+        };
+      })
+    ]);
+  };
+}
