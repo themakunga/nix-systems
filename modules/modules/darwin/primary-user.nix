458669e8ac4882d11ca90d664bdb3bd7301edbd3
Nicolas Martinez V <nmartinezv@icloud.com>
2026-07-06 13:17:41 -0400

    fix: option correct

--- modules/modules/darwin/primary-user.nix
+++ modules/modules/darwin/primary-user.nix
@@ -3,14 +3,15 @@
   config,
   ...
 }: let
-  inherit (lib) types mkEnableOption mkIf;
+  inherit (lib) types mkEnableOption mkOption mkIf;
   cfg = config.my.primaryUser;
 in {
   flake.darwinModules.primaryUser = {
     options.my.primaryUser = {
       enable = mkEnableOption "Define primeary USER for dariwn host";
-      username = {
+      username = mkOption {
         type = types.str;
+        description = "main username to asign";
       };
     };
 
