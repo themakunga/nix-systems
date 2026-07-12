a9801e13170298c697d184edfd9732ecdf0ce2a9
Nicolas Martinez V <nmartinezv@icloud.com>
2026-07-12 00:55:09 -0400

    feat: outerheaven packages

--- modules/profiles/nicolas-work.nix
+++ modules/profiles/nicolas-work.nix
@@ -1,13 +1,5 @@
 {
-  flake.profileModules.nicolas-work = {
-    lib,
-    pkgs,
-    config,
-    ...
-  }: let
-    inherit (lib) mkIf;
-    inherit (pkgs.stdenv.hostPlatform) isDarwin;
-  in {
+  flake.profileModules.nicolas-work = {config, ...}: {
     sops.secrets = {
       "profiles/nicolas-work/ssh/private_key" = {};
       "profiles/nicolas-work/gpg/private_key" = {};
@@ -15,44 +7,66 @@
       "profiles/nicolas-work/gpg/key_id" = {};
     };
 
-    homebrew = mkIf isDarwin {
-      casks = [
-        "firefox"
-        "firefox-dev"
-      ];
-    };
-
-    my.userProfiles.nicolas-work.homeManager = {
-      services.gpg-agent = {
-        enable = true;
-        enableSshSupport = true;
-      };
-
-      programs = {
-        sops.gpg = {
+    my = {
+      apps = {
+        base = {
           enable = true;
-          keys = [
-            {
-              name = "work-key";
-              publicKey = config.sops.secrets."profiles/nicolas-work/gpg/public_key".path;
-              privateKey = config.sops.secrets."profiles/nicolas-work/gpg/private_key".path;
-            }
+          level = "user";
+          targetUser = "nicolas";
+          apps = [
+            "typora"
+            "Termius"
           ];
         };
-        git-identity = {
+        personal = {
           enable = true;
-          workspaces.nicolas-work = {
-            directory = "~/Projects";
-            realName = "Nicolas Villarroel Martinez.";
-            email = "nmartinezv@icloud.com";
-            gpg = {
-              enable = true;
-              keyId =
-                config.sops.secrets."profiles/nicolas-work/gpg/key_id".path;
-            };
-            ssh = {
-              enableAuth = true;
-              privateKey = config.sops.secrets."profiles/nicolas-work/ssh/private_key".path;
+          level = "user";
+          targetUser = "nicolas";
+          apps = [
+            "qmk-toolbox"
+            "via"
+            "nchat"
+            "steam"
+            "Whatsapp Messenger"
+            "goodnotes"
+            "obsidian"
+            "discord"
+          ];
+        };
+      };
+
+      userProfiles.nicolas-work.homeManager = {
+        services.gpg-agent = {
+          enable = true;
+          enableSshSupport = true;
+        };
+
+        programs = {
+          sops.gpg = {
+            enable = true;
+            keys = [
+              {
+                name = "work-key";
+                publicKey = config.sops.secrets."profiles/nicolas-work/gpg/public_key".path;
+                privateKey = config.sops.secrets."profiles/nicolas-work/gpg/private_key".path;
+              }
+            ];
+          };
+          git-identity = {
+            enable = true;
+            workspaces.nicolas-work = {
+              directory = "~/Projects";
+              realName = "Nicolas Villarroel Martinez.";
+              email = "nmartinezv@icloud.com";
+              gpg = {
+                enable = true;
+                keyId =
+                  config.sops.secrets."profiles/nicolas-work/gpg/key_id".path;
+              };
+              ssh = {
+                enableAuth = true;
+                privateKey = config.sops.secrets."profiles/nicolas-work/ssh/private_key".path;
+              };
             };
           };
         };
