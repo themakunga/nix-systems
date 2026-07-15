65d4fb07e39b0dac5f75a156c50eecf8d58579dc
Nicolas Villarroel <nmartinezv@icloud.com>
2026-07-15 14:26:21 -0400

    Update valve.nix

--- modules/hosts/linux/raspberry-pi/valve.nix
+++ modules/hosts/linux/raspberry-pi/valve.nix
@@ -43,7 +43,7 @@ in {
       rpiModules.common
       rpiModules.performance
       rpiModules.hardware-rpi5
-      rpiModules.disko-sd
+      rpiModules.sd-image
       nixosModules.wifi
 
       commonModules.network
