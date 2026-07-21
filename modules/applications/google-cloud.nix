b3ccf662f983a82e0591e5b577c1d5499f40e118
Nicolas Martinez V <nmartinezv@icloud.com>
2026-07-20 21:37:22 -0400

    feat: gemini and google cloud

---
+++ modules/applications/google-cloud.nix
@@ -0,0 +1,52 @@
+# =========================================================
+# Archivo de Configuración de NixOS / Home Manager
+# Repositorio: TheMakunga Infrastructure
+# Módulo auto-gestionado.
+# =========================================================
+{
+  inputs,
+  self,
+  ...
+}: let
+  inherit (inputs) dotfiles;
+  inherit (self.lib) mkAppModule;
+in {
+  flake.applicationModules.google-cloud = {
+    gemini = mkAppModule "gemini-cli" "Enable Gemini CLI and configure it" ({config, ...}: {
+      my.apps.gemini-cli = {
+        level = "system";
+        apps = ["gemini-cli"];
+      };
+
+      sops.secrets."applications.gemini.api-key" = {};
+
+      environment.interactiveShellInit = ''
+        export GEMINI_API_KEY="$( cat ${config.sops.secrets."applications.gemini.api-key".path} 2>/dev/null)"
+      '';
+
+      home-manager.sharedModules = [
+        {
+          xdg.configFile."gemini" = {
+            source = "${dotfiles}/gemini";
+            recursive = true;
+          };
+        }
+      ];
+    });
+    gcloud = mkAppModule "gcloud" "Enable Google Cloud SDK" {
+      my.apps.gcloud = {
+        level = "system";
+        apps = ["google-cloud-sdk"];
+      };
+
+      home-manager.sharedModules = [
+        {
+          xdg.configFile."gcloud" = {
+            source = "${dotfiles}/gcloud";
+            recursive = true;
+          };
+        }
+      ];
+    };
+  };
+}
